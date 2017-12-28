3365ab9b654c216898ef3aa3d376d609d1bff52b
Kent Riemondy <kent.riemondy@gmail.com>
2017-12-28 11:03:07 -0700

    modify source:         - report per strand         - report insertions and deletions separately         - report indels +1 shifted from mpileup         - report only first position of indels         - modify command line arg parsing and reporting

--- mpileup2readcounts.cc
+++ mpileup2readcounts.cc
@@ -1,8 +1,11 @@
 /*  mpileup2readcounts.cc -- Get base counts from mpileup output
+    
+    Modified by Kent Riemondy
 
     Copyright (c) 2016, Avinash Ramu
-
     Author: Avinash Ramu <aramu@genome.wustl.edu>
+    
+    Copyright (c) 2017, Kent Riemondy
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,18 +25,23 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.  */
 
+
 #include <iostream>
 #include <stdexcept>
 #include <sstream>
+#include <map>
+#include <cctype>
+#include <algorithm>
 
 using namespace std;
 
 void usage() {
     std::cerr << "samtools mpileup -f ref.fa -l regions.bed"
-              << " alignments.bam | mpileup2readcounts";
+              << " alignments.bam | mpileup2readcounts - "
+              << endl;
 }
 
-//Convert a number to a string
+//Convert a string to an int
 inline int str_to_num(string num) {
     stringstream ss;
     int num_uint;
@@ -42,93 +50,146 @@ inline int str_to_num(string num) {
     return num_uint;
 }
 
+map<string,string> bp_map() {
+    map<string,string> bps ;    
+    bps["T"] = "A" ; 
+    bps["A"] = "T" ;
+    bps["G"] = "C" ;
+    bps["C"] = "G" ;
+    bps["N"] = "N" ;
+    return bps ;
+}
+
 //DS to hold the pertinent information
 class mpileup_line {
     public:
         string chr;
-        string identifier; //sample identifier
         int pos;
         string ref_base;
         int depth;
         string bases;
         string qual;
         //Counts for different bases
-        int refcount, altcount;
-        int acount, ccount, gcount, tcount, ncount, indelcount;
+        int refcountneg, refcountpos;
+        int pos_depth, neg_depth ; 
+        int acount_pos, ccount_pos, gcount_pos, tcount_pos, ncount_pos;
+        int acount_neg, ccount_neg, gcount_neg, tcount_neg, ncount_neg;
+        int pos_delcount, neg_delcount, pos_inscount, neg_inscount ;
+
+        map<string,string> bps ; 
+
         mpileup_line() {
             chr = ref_base = bases = qual = "NA";
             depth = pos = 0;
-            refcount = altcount = 0;
-            acount = ccount = gcount = tcount = ncount = 0;
-            indelcount = 0;
+            pos_depth = neg_depth = 0;
+            acount_pos = ccount_pos = gcount_pos = tcount_pos = ncount_pos = 0;
+            acount_neg = ccount_neg = gcount_neg = tcount_neg = ncount_neg = 0;
+            refcountpos = refcountneg = 0 ; 
+            pos_inscount = neg_inscount = 0;
+            pos_delcount = neg_delcount = 0;
+            bps = bp_map() ; 
         }
-        //Set the appropriate count for total alt
-        //Needs to be called before set_ref_nuc_count()
-        void set_alt_count() {
-            //This works since ref bases are . and ,
-            altcount = acount + ccount + gcount + tcount +
-                        ncount;
+        
+        void set_depth() {
+            pos_depth = acount_pos + ccount_pos + gcount_pos + tcount_pos +
+                        ncount_pos ; 
+            neg_depth = acount_neg + ccount_neg + gcount_neg + tcount_neg +
+                        ncount_neg ;
         }
+        
         //Set the appropriate count for ref nucleotide
         void set_ref_nuc_count() {
             switch(ref_base[0]) {
                 case 'A':
-                    acount = refcount;
+                    acount_pos = refcountpos;
+                    tcount_neg = refcountneg;
                     break;
                 case 'C':
-                    ccount = refcount;
+                    ccount_pos = refcountpos;
+                    gcount_neg = refcountneg;
                     break;
                 case 'G':
-                    gcount = refcount;
+                    gcount_pos = refcountpos;
+                    ccount_neg = refcountneg;
                     break;
                 case 'T':
-                    tcount = refcount;
+                    tcount_pos = refcountpos;
+                    acount_neg = refcountneg;
                     break;
                 case 'N':
-                    ncount = refcount;
+                    ncount_pos = refcountpos;
+                    ncount_neg = refcountneg;
                     break;
                 //Deal with -,R,Y,K,M,S,W etc
                 default:
                     break;
             }
         }
+
         static void print_header(ostream& out = cout) {
             out << "chr" << "\t"
                 << "pos" << "\t"
+                << "strand" << "\t"
                 << "depth" << "\t"
                 << "ref_base" << "\t"
                 << "refcount" << "\t"
-                << "altcount" << "\t"
                 << "acount" << "\t"
                 << "ccount" << "\t"
                 << "gcount" << "\t"
                 << "tcount" << "\t"
                 << "ncount" << "\t"
-                << "indelcount" << "\t"
-                << "identifier"
+                << "delcount" << "\t"
+                << "inscount"
                 << endl;
         }
-        void print(ostream& out = cout) {
+        void print_pos(ostream& out = cout) {
             out << chr << "\t"
                 << pos << "\t"
-                << depth << "\t"
+                << "+" << "\t"
+                << pos_depth << "\t"
                 << ref_base << "\t"
-                << refcount << "\t"
-                << altcount << "\t"
-                << acount << "\t"
-                << ccount << "\t"
-                << gcount << "\t"
-                << tcount << "\t"
-                << ncount << "\t"
-                << indelcount << "\t"
-                << identifier
+                << refcountpos << "\t"
+                << acount_pos << "\t"
+                << ccount_pos << "\t"
+                << gcount_pos << "\t"
+                << tcount_pos << "\t"
+                << ncount_pos << "\t"
+                << pos_delcount << "\t"
+                << pos_inscount
+                << endl;
+        }
+        void print_neg(ostream& out = cout) {
+            out << chr << "\t"
+                << pos << "\t"
+                << "-" << "\t"
+                << neg_depth << "\t"
+                << bps[ref_base] << "\t"
+                << refcountneg << "\t"
+                << acount_neg << "\t"
+                << ccount_neg << "\t"
+                << gcount_neg << "\t"
+                << tcount_neg << "\t"
+                << ncount_neg << "\t"
+                << neg_delcount << "\t"
+                << neg_inscount
                 << endl;
         }
 };
 
+//Parse the pileup string, keep indel counts to pass to next line
+void parse_bases_to_readcounts(mpileup_line& ml1, 
+                               int& pos_previous_dels,
+                               int& neg_previous_dels,
+                               int& pos_previous_ins,
+                               int& neg_previous_ins) {
+    
+    ml1.neg_delcount = neg_previous_dels ;
+    ml1.pos_delcount = pos_previous_dels ;
+    ml1.neg_inscount = neg_previous_ins ;
+    ml1.pos_inscount = pos_previous_ins ;
 
-//Parse the pileup string
-void parse_bases_to_readcounts(mpileup_line& ml1) {
+    pos_previous_dels = neg_previous_dels = pos_previous_ins = neg_previous_ins = 0;
+
     for(int i = 0; i < ml1.bases.length(); i++) {
         char base = ml1.bases[i];
         string indelsize_string;
@@ -136,47 +197,91 @@ void parse_bases_to_readcounts(mpileup_line& ml1) {
         switch(base) {
             //Match to reference
             case '.':
+                ml1.refcountpos += 1;
+                break;
             case ',':
-                ml1.refcount += 1;
+                ml1.refcountneg += 1;
                 break;
+            
             case 'a':
+                ml1.acount_neg += 1;
+                break;
             case 'A':
-                ml1.acount += 1;
+                ml1.acount_pos += 1;
                 break;
+
             case 'c':
+                ml1.ccount_neg += 1;
+                break;
             case 'C':
-                ml1.ccount += 1;
+                ml1.ccount_pos += 1;
                 break;
+
             case 'g':
+                ml1.gcount_neg += 1;
+                break;
             case 'G':
-                ml1.gcount += 1;
+                ml1.gcount_pos += 1;
                 break;
+
             case 't':
+                ml1.tcount_neg += 1;
+                break;
             case 'T':
-                ml1.tcount += 1;
+                ml1.tcount_pos += 1;
                 break;
+
             case 'n':
-            case 'N':
-                ml1.ncount += 1;
+                ml1.ncount_neg += 1;
                 break;
+            case 'N':
+                ml1.ncount_pos += 1;
+                break;
+
             //Reference skips
             case '<':
             case '>':
                 break;
             //This base is deleted
             case '*':
-                ml1.indelcount += 1;
                 break;
-            //Insertion or deletion
-            case '-':
-            case '+':
-                ml1.indelcount += 1;
-                i++;
-                while(ml1.bases[i] >= 48 &&
-                      ml1.bases[i] <= 57) {
+            
+            //Insertion 
+            case '+': 
+                i++; // advance past (+ or -)
+                while(ml1.bases[i] >= 48 && // A char type of 0 is typecast to 48 as int
+                      ml1.bases[i] <= 57) { // a char type of 9 is typecase to 57 as int
                     indelsize_string = indelsize_string + ml1.bases[i];
                     i = i+1;
                 }
+
+                // check for upper or lower (upper = + strand)
+                if (isupper(ml1.bases[i])){
+                    pos_previous_ins += 1 ;
+                } else {
+                    neg_previous_ins += 1 ;
+                }
+
+                indelsize_int = str_to_num(indelsize_string);
+                i += indelsize_int - 1;
+                break;
+
+            //Deletion
+            case '-':
+                i++; // advance past (+ or -)
+                while(ml1.bases[i] >= 48 && // A char type of 0 is typecast to 48 as int
+                      ml1.bases[i] <= 57) { // a char type of 9 is typecase to 57 as int
+                    indelsize_string = indelsize_string + ml1.bases[i];
+                    i = i+1;
+                }
+
+                // check for upper or lower (upper = + strand)
+                if (isupper(ml1.bases[i])){
+                    pos_previous_dels += 1 ;
+                } else {
+                    neg_previous_dels += 1 ;
+                }
+
                 indelsize_int = str_to_num(indelsize_string);
                 i += indelsize_int - 1;
                 break;
@@ -188,43 +293,78 @@ void parse_bases_to_readcounts(mpileup_line& ml1) {
                 i = i + 1;//Skip quality
                 break;
             default:
-                throw runtime_error("Unknown ref base: " +
-                                    base);
+                throw runtime_error("Unknown ref base: " + to_string(base));
         }
     }
-    ml1.set_alt_count();
+
     ml1.set_ref_nuc_count();
+    ml1.set_depth();
 }
 
 //Split the line into the required fields and parse
-void process_mpileup_line(std::string line, std::string id1) {
+void process_mpileup_line(std::string line, 
+                         int& pos_previous_dels,
+                         int& neg_previous_dels,
+                         int& pos_previous_ins,
+                         int& neg_previous_ins) {
     stringstream ss(line);
     mpileup_line ml1;
-    ml1.identifier = id1;
     string pos, depth;
+
     getline(ss, ml1.chr, '\t');
+    
     getline(ss, pos, '\t');
     ml1.pos = str_to_num(pos);
-    getline(ss, ml1.ref_base, '\t');
+    
+    string base;
+    getline(ss, base, '\t');
+    transform(base.begin(), base.end(), base.begin(), ::toupper) ;
+    ml1.ref_base = base;
+    
     getline(ss, depth, '\t');
     ml1.depth = str_to_num(depth);
+    
     getline(ss, ml1.bases, '\t');
     getline(ss, ml1.qual, '\t');
-    parse_bases_to_readcounts(ml1);
-    ml1.print();
+    
+    parse_bases_to_readcounts(ml1, 
+                              pos_previous_dels,
+                              neg_previous_dels,
+                              pos_previous_ins,
+                              neg_previous_ins);
+   
+    if(ml1.pos_depth > 0) ml1.print_pos();
+    if(ml1.neg_depth > 0) ml1.print_neg();
+    
 }
 
 int main(int argc, char* argv[]) {
+    
+    if(argc > 1 && string(argv[1]) != "-") {
+        usage() ; 
+        return 1;
+    } 
+    
+    if(argc == 1) {
+        usage() ; 
+        return 1;
+    }
+    
     string line;
     mpileup_line::print_header();
     getline(cin, line);
-    string identifier = "NA";
-    if(argc > 1) {
-        identifier = argv[1];
-    }
+    
+    int pos_previous_dels, neg_previous_dels, pos_previous_ins, neg_previous_ins;
+    pos_previous_dels = neg_previous_dels = pos_previous_ins = neg_previous_ins = 0;
+    
     while(cin) {
         try {
-            process_mpileup_line(line, identifier);
+            // store indel count and add to next line
+            process_mpileup_line(line, 
+                                 pos_previous_dels,
+                                 neg_previous_dels,
+                                 pos_previous_ins,
+                                 neg_previous_ins);
         } catch(const std::runtime_error& e) {
             cerr << e.what() << endl;
             cerr << "\nError parsing line " << line;
